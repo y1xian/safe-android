8cb7b3b08d1293c32ffa2664bf18eec275048152
yyx <code_y@163.com>
2018-06-23 19:41:36 +0800

    更新 -> 禁止滑动viewpage

--- yyxframe/src/main/java/com/yyx/yyxframe/widget/YNoScrollViewPager.java
+++ yyxframe/src/main/java/com/yyx/yyxframe/widget/YNoScrollViewPager.java
@@ -9,7 +9,8 @@ import android.view.MotionEvent;
  * 提供禁止滑动功能的自定义ViewPager
  */
 public class YNoScrollViewPager extends ViewPager {
-    private boolean noScroll = false;
+    private boolean isCanScroll = true;
+    private boolean isHasScrollAnim=true;
 
     public YNoScrollViewPager(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -20,8 +21,20 @@ public class YNoScrollViewPager extends ViewPager {
         super(context);
     }
 
-    public void setNoScroll(boolean noScroll) {
-        this.noScroll = noScroll;
+    /**
+     * 设置其是否能滑动
+     * @param isCanScroll false 禁止滑动， true 可以滑动
+     */
+    public void setNoScroll(boolean isCanScroll) {
+        this.isCanScroll = isCanScroll;
+    }
+
+    /**
+     *  设置其是否有动画效果
+     * @param isHasScrollAnim false 无动画 true 有动画
+     */
+    public void setHasScrollAnim(boolean isHasScrollAnim){
+        this.isHasScrollAnim=isHasScrollAnim;
     }
 
     @Override
@@ -31,12 +44,12 @@ public class YNoScrollViewPager extends ViewPager {
 
     @Override
     public boolean onTouchEvent(MotionEvent arg0) {
-        return !noScroll && super.onTouchEvent(arg0);
+        return !isCanScroll && super.onTouchEvent(arg0);
     }
 
     @Override
     public boolean onInterceptTouchEvent(MotionEvent arg0) {
-        return !noScroll && super.onInterceptTouchEvent(arg0);
+        return !isCanScroll && super.onInterceptTouchEvent(arg0);
     }
 
     @Override
@@ -44,9 +57,13 @@ public class YNoScrollViewPager extends ViewPager {
         super.setCurrentItem(item, smoothScroll);
     }
 
+    /**
+     * 设置其是否去求切换时的滚动动画
+     *isHasScrollAnim为false时，会去除滚动效果
+     */
     @Override
     public void setCurrentItem(int item) {
-        super.setCurrentItem(item);
+        super.setCurrentItem(item,isHasScrollAnim);
     }
 
 }
